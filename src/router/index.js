5f68c691902d238c4e9cdde7ff085a21dc24eb41
Tianze Ds Qiu <slaaker@126.com>
2021-07-25 13:00:49 +0800

    fix a bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
- 
-Vue.component('VueCtkDateTimePicker', VueCtkDateTimePicker);
 Vue.use(VueRouter)
 
 const routes = [
